91d6717b0a77b756a07c896ff58e6dc2bd9abe34
kedz-1 <khadar7@hotmail.co.uk>
2026-07-30 16:53:03 +0100

    added a "main" function to allow for utilisation of the class StarWarsConverter

--- src/starwars/main.py
+++ src/starwars/main.py
@@ -2,14 +2,26 @@ from src.starwars.load import load_json
 from src.starwars.write import write_csv
 from dotenv import load_dotenv
 import os
+from class_converter import StarWarsConverter
 
 load_dotenv()
 file_path = os.getenv('FILE_PATH')
 
-def convert_to_csv(input_file, output_file):
+# def convert_to_csv(input_file, output_file):
 
-    data = load_json(input_file)
-    response = write_csv(data, output_file)
+#     data = load_json(input_file)
+#     response = write_csv(data, output_file)
 
 
 # print(convert_to_csv(file_path, "characters.csv"))
+
+def main():
+    converter = StarWarsConverter(file_path, 'characters.csv')
+
+    converter.convert_to_csv()
+
+print(main())
+
+'''
+both of these functions work, one utilised function based programming whilst the other uses the StarWarsConverter class
+'''
